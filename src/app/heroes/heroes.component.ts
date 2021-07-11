2c6fba90ecafb6cce63d102054097d09b0ffe93e
mikael epelbaum <mikaelepelbaum@gmail.com>
2021-07-11 22:27:17 +0300

    second level

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,17 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-  constructor() {
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  heroes = HEROES;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
