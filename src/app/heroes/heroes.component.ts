9aaaf8c77352c13118cfebcee6ab355bca3d5085
mikael epelbaum <mikaelepelbaum@gmail.com>
2021-07-11 23:02:20 +0300

    fourth level

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,25 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
-  constructor() {}
+  heroes: Hero[] = [];
 
-  ngOnInit(): void { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
   
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 }
 }
